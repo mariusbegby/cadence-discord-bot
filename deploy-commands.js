cc39df1c9172bda6293c53d8080aacfea87bcfe5
Marius Begby <mariusbegby@gmail.com>
2023-07-04 15:42:23 +0200

    Added script to deploy commands from folder

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+const { REST, Routes } = require('discord.js');
+const { token, clientId } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs
+    .readdirSync('./commands')
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log("Commands: ", commands.map((command) => {return command.name}));
+
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(clientId), {
+            body: commands
+        });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
