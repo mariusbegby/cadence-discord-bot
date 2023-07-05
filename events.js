23e9441d529337c7971b743cda937890b20cf557
Marius Begby <mariusbegby@gmail.com>
2023-07-05 12:34:46 +0200

    Remove unused file

--- events.js
+++
@@ -1,5 +0,0 @@
-// this event is emitted whenever discord-player starts to play a track
-player.events.on('playerStart', (queue, track) => {
-    // we will later define queue.metadata object while creating the queue
-    queue.metadata.channel.send(`Started playing **${track.title}**!`);
-});
