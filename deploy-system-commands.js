9feff8c62f849a6e56a9c78ea75073aa66b6aee0
Marius Begby <mariusbegby@gmail.com>
2023-07-07 00:29:27 +0200

    Update deploy commands

---
+++ deploy-system-commands.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const { REST, Routes } = require('discord.js');
+const { token, clientId, systemServerGuildId } = require('./config.json');
+
+const systemCommands = [];
+const systemCommandFiles = fs
+    .readdirSync('./system-commands')
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of systemCommandFiles) {
+    const systemCommand = require(`./system-commands/${file}`);
+    systemCommands.push(systemCommand.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log("DEPLOYING SYSTEM SLASH COMMANDS");
+        console.log("System Commands: ", systemCommands.map((systemCommand) => {return systemCommand.name}));
+
+        console.log('Started refreshing application (/) system commands.');
+
+        await rest.put(Routes.applicationGuildCommands(clientId, systemServerGuildId), {
+            body: systemCommands
+        });
+
+        console.log('Successfully reloaded application (/) system commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
