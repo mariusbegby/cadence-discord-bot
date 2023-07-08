9863340bed00d76e07f7683b64d0217849bb55e0
Marius Begby <mariusbegby@gmail.com>
2023-07-08 17:34:40 +0200

    Updated /help command with new /remove command

--- commands/help.js
+++ commands/help.js
@@ -13,13 +13,14 @@ module.exports = {
                 new EmbedBuilder()
                     .setDescription(
                         `**List of commands**\n` +
-                        `\`/help\` - Show this menu.\n` +
-                        `\`/play\` \`query\` - Add a track to the queue by search query or url.\n` +
-                        `\`/pause\` - Pause or resume the current track.\n` +
-                        `\`/skip\` - Skip the current track.\n` +
-                        `\`/leave\` - Remove bot from voice channel and clear queue.\n` +
-                        `\`/queue\` - Show the list of tracks added to the queue.\n` +
-                        `\`/volume\` \`[percentage]\` - Show or set the playback volume for tracks.\n`
+                            `\`/help\` - Show this menu.\n` +
+                            `\`/play\` \`query\` - Add a track to the queue by search query or url.\n` +
+                            `\`/pause\` - Pause or resume the current track.\n` +
+                            `\`/skip\` - Skip the current track.\n` +
+                            `\`/remove\` - Remove a track from the queue.\n` +
+                            `\`/leave\` - Remove bot from voice channel and clear queue.\n` +
+                            `\`/queue\` - Show the list of tracks added to the queue.\n` +
+                            `\`/volume\` \`[percentage]\` - Show or set the playback volume for tracks.\n`
                     )
                     .setColor(embedColors.colorInfo)
             ]
