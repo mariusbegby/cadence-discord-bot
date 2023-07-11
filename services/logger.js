6bb602ebbb655373c61de96b592a0de40585aa72
Marius Begby <mariusbegby@gmail.com>
2023-07-11 16:10:09 +0200

    feat: Log events to file based on log level

--- services/logger.js
+++ services/logger.js
@@ -1,12 +1,35 @@
 const pino = require('pino');
-const { minimumLogLevel } = require('../config.json');
+const { minimumLogLevel, minimumLogLevelConsole } = require('../config.json');
 
-module.exports = pino({
-    level: minimumLogLevel,
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            ignore: 'pid,hostname'
+const transport = pino.transport({
+    targets: [
+        {
+            target: 'pino/file',
+            level: 'debug',
+            options: { destination: `./app-debug.log` }
+        },
+        {
+            target: 'pino/file',
+            level: 'info',
+            options: { destination: `./app-info.log` }
+        },
+        {
+            target: 'pino/file',
+            level: 'error',
+            options: { destination: `./app-error.log` }
+        },
+        {
+            level: minimumLogLevelConsole,
+            target: 'pino-pretty'
         }
-    }
+    ]
 });
+
+module.exports = pino(
+    {
+        level: minimumLogLevel,
+        timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+        base: undefined
+    },
+    transport
+);
