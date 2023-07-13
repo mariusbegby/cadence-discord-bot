c909afb9c023fcebeb8ad163f1716059ea3b1a36
Marius Begby <mariusbegby@gmail.com>
2023-07-13 13:28:18 +0200

    feat: Updated /status system command with more details

--- system-commands/status.js
+++ system-commands/status.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
 const { embedColors, systemServerGuildIds } = require('../config.json');
+const osu = require('node-os-utils');
+const packageJson = require('../package.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,13 +22,27 @@ module.exports = {
             });
         }
 
-        let uptimeInSeconds = process.uptime();
+        const interactionLatency = Date.now() - interaction.createdTimestamp;
+        const uptimeInSeconds = process.uptime();
         let uptimeDate = new Date(0);
         uptimeDate.setSeconds(uptimeInSeconds.toFixed(0));
-        let uptimeString = uptimeDate.toISOString().substring(11, 19);
-
+        const uptimeString = uptimeDate.toISOString().substring(11, 19);
+        const nodeProcessMemUsageInMb = (
+            process.memoryUsage().heapUsed /
+            1024 /
+            1024
+        ).toFixed(2);
+        const usedMemoryInMB = Math.ceil(
+            (await osu.mem.info()).usedMemMb
+        ).toLocaleString('en-US');
+        const cpuUsage = await osu.cpu.usage();
+        const cpuCores = await osu.cpu.count();
+        const platform = await osu.os.platform();
+        const releaseVersion = packageJson.version;
+        const discordJsVersion = packageJson.dependencies['discord.js'];
+        const discordPlayerVersion = packageJson.dependencies['discord-player'];
+        const discordApiLatency = client.ws.ping;
         let activeVoiceConnections = 0;
-
         client.guilds.cache.forEach((guild) => {
             const queue = useQueue(guild.id);
 
@@ -39,22 +55,21 @@ module.exports = {
             embeds: [
                 new EmbedBuilder()
                     .setDescription(
-                        '**Bot Status**\n' +
+                        '**System and bot status**\n' +
                             `Uptime: \`${uptimeString}\`\n` +
-                            `Memory Usage: \`${(
-                                process.memoryUsage().heapUsed /
-                                1024 /
-                                1024
-                            ).toFixed(2)} MB\`\n` +
-                            `Node Version: \`${process.version}\`\n` +
-                            `Discord.js Version: \`v${
-                                require('discord.js').version
-                            }\`\n` +
-                            `Discord Player Version: \`v${
-                                require('discord-player').version
-                            }\`\n` +
-                            `Joined guilds: \`${client.guilds.cache.size}\`` +
-                            `\nActive voice connections: \`${activeVoiceConnections}\``
+                            `Platform: \`${platform}\`\n` +
+                            `CPU cores: \`${cpuCores}\`\n` +
+                            `CPU usage: \`${cpuUsage}%\`\n` +
+                            `Node.js mem usage: \`${nodeProcessMemUsageInMb} MB\`\n` +
+                            `System mem usage: \`${usedMemoryInMB} MB\`\n` +
+                            `Bot version: \`v${releaseVersion}\`\n` +
+                            `Node.js version: \`${process.version}\`\n` +
+                            `Discord.js version: \`${discordJsVersion}\`\n` +
+                            `Discord-player version: \`${discordPlayerVersion}\`\n` +
+                            `Discord API latency: \`${discordApiLatency} ms\`\n` +
+                            `Interaction latency: \`${interactionLatency} ms\`\n` +
+                            `Active voice connections: \`${activeVoiceConnections}\`\n` +
+                            `Joined guilds: \`${client.guilds.cache.size}\``
                     )
                     .setColor(embedColors.colorInfo)
             ]
