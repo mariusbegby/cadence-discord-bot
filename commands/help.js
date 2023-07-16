2f1974d2ba7f12f5ab3fb67f99066d3fb81221d9
Marius Begby <mariusbegby@gmail.com>
2023-07-16 18:08:18 +0200

    fix: Updated /help with seek command

--- commands/help.js
+++ commands/help.js
@@ -17,8 +17,9 @@ module.exports = {
                             '- `/pause` - Pause or resume the current track.\n' +
                             '- `/queue` `[page]` - Show the list of tracks added to the queue.\n' +
                             `- \`/nowplaying\` - ${embedIcons.new} Show information about the track currently playing.\n` +
+                            `- \`/seek\` - ${embedIcons.beta} Seek to a specified duration in the current track.\n` +
                             `- \`/loop\` \`mode\` - ${embedIcons.beta} Toggle looping a track, the whole queue or autoplay.\n` +
-                            `- \`/filters\` - ${embedIcons.beta} Toggle various audio filters during playback.\n` +
+                            '- `/filters` - Toggle various audio filters during playback.\n' +
                             '- `/volume` `[percentage]` - Show or set the playback volume for tracks.\n' +
                             '- `/skip` `[trackNumber]` - Skip to next or specified track.\n' +
                             '- `/remove` `trackNumber` - Remove specified track from the queue.\n' +
