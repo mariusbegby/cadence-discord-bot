0bdf572d0de54b06bf4bc6cc9e2fa2b900ce1c55
Marius Begby <mariusbegby@gmail.com>
2023-07-19 00:06:35 +0200

    feat: Added metadata to queue, and added more logging and handling of error events

--- src/events/player/generalError.js
+++ src/events/player/generalError.js
@@ -2,19 +2,19 @@ const logger = require('../../services/logger');
 const { embedOptions, systemOptions, botOptions } = require('../../config');
 const { EmbedBuilder } = require('discord.js');
 
-// Emitted when the audio player fails to load the stream for a track
+// Emitted when the player queue encounters error (general error with queue)
 module.exports = {
-    name: 'playerSkip',
+    name: 'error',
     isDebug: false,
     isPlayerEvent: true,
-    execute: async (queue, track) => {
-        logger.error(`player.events.on('playerSkip'): Failed to play '${track.url}'.`);
+    execute: async (queue, error) => {
+        logger.error(error, 'player.events.on(\'error\'): Player queue encountered error event');
 
         await queue.metadata.channel.send({
             embeds: [
                 new EmbedBuilder()
                     .setDescription(
-                        `**${embedOptions.icons.error} Uh-oh... _Something_ went wrong!**\nIt seems there was an issue while loading stream for the track.\n\nIf you performed a command, you can try again.\n\n_If this problem persists, please submit a bug report in the **[support server](${botOptions.serverInviteUrl})**._`
+                        `**${embedOptions.icons.error} Uh-oh... _Something_ went wrong!**\nIt seems there was an issue related to the queue or current track.\n\nIf you performed a command, you can try again.\n\n_If this problem persists, please submit a bug report in the **[support server](${botOptions.serverInviteUrl})**._`
                     )
                     .setColor(embedOptions.colors.error)
             ]
@@ -25,7 +25,7 @@ module.exports = {
                 embeds: [
                     new EmbedBuilder()
                         .setDescription(
-                            `${embedOptions.icons.error} **player.events.on('playerSkip')**\n${track.url}` +
+                            `${embedOptions.icons.error} **player.events.on('error')**\n${error.message}` +
                                 `\n\n<@${systemOptions.systemUserId}>`
                         )
                         .setColor(embedOptions.colors.error)
--- src/events/player/playerError.js
+++ src/events/player/playerError.js
@@ -1,11 +1,38 @@
 const logger = require('../../services/logger');
+const { embedOptions, systemOptions, botOptions } = require('../../config');
+const { EmbedBuilder } = require('discord.js');
 
+// Emitted when the player encounters an error while streaming audio track
 module.exports = {
     name: 'error',
     isDebug: false,
     isPlayerEvent: true,
     execute: async (queue, error) => {
-        logger.error(error, 'Player queue error event');
-        process.env.NODE_ENV === 'development' ? logger.trace(queue) : null;
+        logger.error(error, 'player.events.on(\'playerError\'): Player error while streaming track');
+
+        await queue.metadata.channel.send({
+            embeds: [
+                new EmbedBuilder()
+                    .setDescription(
+                        `**${embedOptions.icons.error} Uh-oh... _Something_ went wrong!**\nIt seems there was an issue while streaming the track.\n\nIf you performed a command, you can try again.\n\n_If this problem persists, please submit a bug report in the **[support server](${botOptions.serverInviteUrl})**._`
+                    )
+                    .setColor(embedOptions.colors.error)
+            ]
+        });
+
+        if (systemOptions.systemMessageChannelId && systemOptions.systemUserId) {
+            await queue.metadata.client.channels.cache.get(systemOptions.systemMessageChannelId).send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            `${embedOptions.icons.error} **player.events.on('playerError')**\n${error.message}` +
+                                `\n\n<@${systemOptions.systemUserId}>`
+                        )
+                        .setColor(embedOptions.colors.error)
+                ]
+            });
+        }
+
+        process.env.NODE_ENV === 'development' ? logger.trace(queue, 'Queue object') : null;
     }
 };
