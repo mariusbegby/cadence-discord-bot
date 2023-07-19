4e8145151fe9b7595036ffcd849836c080c198b9
Marius Begby <mariusbegby@gmail.com>
2023-07-19 16:20:13 +0200

    fix: Added try/catch around createClient and createPlayer factory functions

--- src/utils/factory/createPlayer.js
+++ src/utils/factory/createPlayer.js
@@ -3,38 +3,43 @@ const { Player, onBeforeCreateStream } = require('discord-player');
 const { stream } = require('yt-stream');
 
 exports.createPlayer = async (client) => {
-    logger.debug('Creating discord-player player...');
+    try {
+        logger.info('Creating discord-player player...');
 
-    const player = new Player(client, {
-        useLegacyFFmpeg: false,
-        ytdlOptions: {
-            quality: 'highestaudio',
-            highWaterMark: 1 << 25,
-            requestOptions: {
-                headers: {
-                    cookie: process.env.YOUTUBE_COOKIE || ''
+        const player = new Player(client, {
+            useLegacyFFmpeg: false,
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25,
+                requestOptions: {
+                    headers: {
+                        cookie: process.env.YOUTUBE_COOKIE || ''
+                    }
                 }
             }
-        }
-    });
+        });
 
-    onBeforeCreateStream(async (track) => {
-        if (track.source === 'youtube') {
-            return (
-                await stream(track.url, {
-                    type: 'audio',
-                    quality: 'high',
-                    highWaterMark: 1 << 25,
-                    cookie: process.env.YOUTUBE_COOKIE || ''
-                })
-            ).stream;
-        }
+        onBeforeCreateStream(async (track) => {
+            if (track.source === 'youtube') {
+                return (
+                    await stream(track.url, {
+                        type: 'audio',
+                        quality: 'high',
+                        highWaterMark: 1 << 25,
+                        cookie: process.env.YOUTUBE_COOKIE || ''
+                    })
+                ).stream;
+            }
 
-        return null;
-    });
+            return null;
+        });
 
-    await player.extractors.loadDefault();
-    logger.debug(`discord-player loaded dependencies:\n${player.scanDeps()}`);
+        await player.extractors.loadDefault();
+        logger.debug(`discord-player loaded dependencies:\n${player.scanDeps()}`);
 
-    return player;
+        return player;
+    } catch (error) {
+        logger.error('Failed to create discord-player player', error);
+        throw error;
+    }
 };
