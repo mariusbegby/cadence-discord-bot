b95be75768ce41558010a3c0d7128c2bb2a12d40
Marius Begby <mariusbegby@gmail.com>
2023-07-19 15:27:07 +0200

    fix: updated logging service to log levels separately

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,53 +1,52 @@
 const pino = require('pino');
 const { loggerOptions } = require('../config');
 
-const transport = pino.transport({
-    targets: [
-        {
-            target: 'pino/file',
-            level: loggerOptions.minimumLogLevel,
-            options: {
-                destination: './logs/app-all.log',
-                mkdir: true,
-                sync: false,
-                minLength: 4096
-            }
-        },
-        {
-            target: 'pino/file',
-            level: 'info',
-            options: {
-                destination: './logs/app-info.log',
-                mkdir: true,
-                sync: false,
-                minLength: 4096
-            }
-        },
-        {
-            target: 'pino/file',
-            level: 'error',
-            options: {
-                destination: './logs/app-error.log',
-                mkdir: true,
-                sync: false
-            }
-        },
-        {
-            target: 'pino/file',
-            level: loggerOptions.minimumLogLevelConsole,
-            options: {
-                colorize: true,
-                sync: false
-            }
-        }
-    ]
-});
-
-module.exports = pino(
+const targets = [
     {
+        target: 'pino/file',
         level: loggerOptions.minimumLogLevel,
-        timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
-        base: undefined
+        options: {
+            destination: './logs/app-all.log',
+            mkdir: true,
+            sync: false,
+            minLength: 4096
+        }
     },
-    transport
-);
+    {
+        target: 'pino/file',
+        level: pino.levels.values.info,
+        options: {
+            destination: './logs/app-info.log',
+            mkdir: true,
+            sync: false,
+            minLength: 4096
+        }
+    },
+    {
+        target: 'pino/file',
+        level: pino.levels.values.error,
+        options: {
+            destination: './logs/app-error.log',
+            mkdir: true,
+            sync: false
+        }
+    },
+    {
+        target: 'pino/file',
+        level: loggerOptions.minimumLogLevelConsole,
+        options: {
+            colorize: true,
+            sync: false
+        }
+    }
+];
+
+const transport = pino.transport({ targets });
+
+const logLevelConfig = {
+    level: loggerOptions.minimumLogLevel,
+    timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+    base: undefined
+};
+
+module.exports = pino(logLevelConfig, transport);
