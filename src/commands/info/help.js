0887a47af94d3bc4b2e6865de8d41040c5369a24
Marius Begby <mariusbegby@gmail.com>
2023-07-27 23:03:01 +0200

    chore: remove comment and rewrite text

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -31,37 +31,11 @@ module.exports = {
                     .setDescription(
                         `${embedOptions.icons.rule} **List of commands**\n` +
                             `${commandListString}\n\n` +
-                            `${embedOptions.icons.support} **Support server**\nJoin the support server for help or suggestions: \n**${botOptions.serverInviteUrl}**\n\n` +
+                            `${embedOptions.icons.support} **Support server**\nJoin the support server for help or to suggest improvements: \n**${botOptions.serverInviteUrl}**\n\n` +
                             `${embedOptions.icons.bot} **Enjoying ${botOptions.name}?**\nAdd me to another server: \n**[Click me!](${botOptions.botInviteUrl})**`
                     )
                     .setColor(embedOptions.colors.info)
             ]
         });
-
-        /*
-        return await interaction.editReply({
-            embeds: [
-                new EmbedBuilder()
-                    .setDescription(
-                        `${embedOptions.icons.rule} **List of commands**\n` +
-                            '- **`/play` `query`** - Add a track or playlist to the queue by url or search.\n' +
-                            '- **`/pause`** - Pause or resume the current track.\n' +
-                            '- **`/queue` `[page]`** - Show the list of tracks added to the queue.\n' +
-                            '- **`/nowplaying`** - Show information about the track currently playing.\n' +
-                            '- **`/seek` `duration`** - Seek to a specified duration in the current track.\n' +
-                            `- **\`/loop\` \`mode\`** - ${embedOptions.icons.beta} Toggle looping a track, the whole queue or autoplay.\n` +
-                            `- **\`/filters\`** - ${embedOptions.icons.beta} Toggle various audio filters during playback.\n` +
-                            '- **`/volume` `[percentage]`** - Show or set the playback volume for tracks.\n' +
-                            '- **`/skip` `[tracknumber]`** - Skip to next or specified track.\n' +
-                            '- **`/remove` `tracknumber`** - Remove specified track from the queue.\n' +
-                            `- **\`/stop\`** - ${embedOptions.icons.new} Stop playing audio and clear the track queue.\n` +
-                            '- **`/leave`** - Clear the queue and remove the bot from voice channel.\n\n' +
-                            `${embedOptions.icons.support} **Support server**\nJoin the support server for help or suggestions: \n**${botOptions.serverInviteUrl}**\n\n` +
-                            `${embedOptions.icons.bot} **Enjoying ${botOptions.name}?**\nAdd me to another server: \n**[Click me!](${botOptions.botInviteUrl})**`
-                    )
-                    .setColor(embedOptions.colors.info)
-            ]
-        });
-        */
     }
 };
