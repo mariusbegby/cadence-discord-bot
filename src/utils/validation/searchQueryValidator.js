4d9353d43afc26993e3c5429abe8ecc781ef7317
Marius Begby <mariusbegby@gmail.com>
2023-08-25 22:33:19 +0200

    fix: Improvements to spotify country-based url filter and playlist support

--- src/utils/validation/searchQueryValidator.js
+++ src/utils/validation/searchQueryValidator.js
@@ -5,15 +5,16 @@ exports.transformQuery = async (query) => {
         try {
             // regex to check for country-based Spotify url
             const regex = new RegExp(
-                'https://open.spotify.com/((?:[a-z]{2,4}-[a-z]{2,4}/)|(?:[a-z]{2,4}/))?track/([a-zA-Z0-9]+)'
+                'https://open.spotify.com/((?:[a-z]{2,4}-[a-z]{2,4}/)|(?:[a-z]{2,4}/))(track|playlist)/([a-zA-Z0-9]+)'
             );
             const isCountryBased = regex.test(query);
 
             // if country-based, transform to normal Spotify url
             if (isCountryBased) {
                 logger.debug(`Transforming country-based Spotify url to normal Spotify url from query ${query}.`);
-                const trackId = query.match(regex)[2];
-                query = `https://open.spotify.com/track/${trackId}`;
+                const type = query.match(regex)[2];
+                const trackId = query.match(regex)[3];
+                query = `https://open.spotify.com/${type}/${trackId}`;
             }
         } catch (error) {
             logger.error(error);
