aa3692592dc876c10b0372df399c1fb0925359ec
Marius Begby <mariusbegby@gmail.com>
2023-12-19 13:11:45 +0100

    fix: override willAutoPlay event to set bot user as requestedBy

---
+++ src/events/player/willAutoPlay.ts
@@ -0,0 +1,31 @@
+import { Track } from 'discord-player';
+import { randomUUID as uuidv4 } from 'node:crypto';
+import { Logger } from 'pino';
+import loggerModule from '../../services/logger';
+import { ExtendedGuildQueuePlayerNode } from '../../types/eventTypes';
+import { User } from 'discord.js';
+
+// Emitted when the audio player will add a new track to the queue from autoplay.
+module.exports = {
+    name: 'willAutoPlay',
+    isDebug: false,
+    isPlayerEvent: true,
+    execute: async (queue: ExtendedGuildQueuePlayerNode, tracks: Track[], done: (track: Track) => void) => {
+        const executionId: string = uuidv4();
+        const logger: Logger = loggerModule.child({
+            module: 'event',
+            name: 'willAutoPlay',
+            executionId: executionId,
+            shardId: queue.metadata?.client.shard?.ids[0],
+            guildId: queue.metadata?.channel.guild.id
+        });
+
+        const [track] = tracks;
+
+        track.requestedBy = queue.metadata?.client.user as User;
+
+        done(track);
+
+        logger.debug(`Autoplay track [${track.url}] changed requestedBy to bot`);
+    }
+};
