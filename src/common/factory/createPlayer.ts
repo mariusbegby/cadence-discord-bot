bf9c662835a344c2f9fe803f72b6694066ffad83
Marius Begby <mariusbegby@gmail.com>
2024-07-10 10:39:42 +0200

    Implement new youtube extractor 'discord-player-youtubei' to resolve issues with youtube playback

--- src/common/factory/createPlayer.ts
+++ src/common/factory/createPlayer.ts
@@ -1,7 +1,10 @@
+/* eslint-disable camelcase */
 import config from 'config';
 import { IPRotationConfig, Player } from 'discord-player';
+import { SpotifyExtractor } from '@discord-player/extractor';
 import { loggerService, Logger } from '../services/logger';
 import { CreatePlayerParams } from '../../types/playerTypes';
+import { YoutubeiExtractor, createYoutubeiStream } from 'discord-player-youtubei';
 
 export const createPlayer = async ({ client, executionId }: CreatePlayerParams): Promise<Player> => {
     const logger: Logger = loggerService.child({
@@ -31,12 +34,29 @@ export const createPlayer = async ({ client, executionId }: CreatePlayerParams):
             }
         });
 
+        // TEMPORARY FIX
+        // Need error handling
+        await player.extractors.register(YoutubeiExtractor, {
+            authentication: {
+                access_token: process.env.YT_ACCESS_TOKEN || '',
+                refresh_token: process.env.YT_REFRESH_TOKEN || '',
+                scope: 'https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube-paid-content',
+                token_type: 'Bearer',
+                expiry_date: '2024-07-10T11:37:01.093Z'
+            }
+        });
+
+        await player.extractors.register(SpotifyExtractor, {
+            createStream: createYoutubeiStream
+        });
+
         // make player accessible from anywhere in the application
         // primarily to be able to use it in broadcastEval and other sharding methods
         // @ts-ignore
         global.player = player;
 
         await player.extractors.loadDefault();
+        await player.extractors.loadDefault((ext) => !['YouTubeExtractor', 'SpotifyExtractor'].includes(ext));
         logger.trace(`discord-player loaded dependencies:\n${player.scanDeps()}`);
 
         return player;
