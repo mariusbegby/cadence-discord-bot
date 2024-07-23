5724e80ea4d0f4abbff2aed113279732b856302d
Marius Begby <mariusbegby@gmail.com>
2024-07-23 15:38:37 +0200

    Add common mock directory for tests

---
+++ src/__tests__/_mocks/MockLoggerService.ts
@@ -0,0 +1,7 @@
+export const MockLoggerService = {
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    setContext: jest.fn()
+};
--- src/__tests__/services/insights/HealthCheckService.test.ts
+++ src/__tests__/services/insights/HealthCheckService.test.ts
@@ -1,33 +1,28 @@
+import { MockLoggerService } from '@mocks/MockLoggerService';
 import { HealthCheckService } from '@services/insights/HealthCheckService';
 import { IHealthCheck } from '@type/insights/IHealthCheck';
 import { ILoggerService } from '@type/insights/ILoggerService';
 
 describe('HealthCheckService', () => {
-    let loggerServiceMock: ILoggerService;
+    let mockLoggerService: ILoggerService = MockLoggerService;
     let healthCheckService: HealthCheckService;
 
     beforeEach(() => {
-        loggerServiceMock = {
-            debug: jest.fn(),
-            info: jest.fn(),
-            warn: jest.fn(),
-            error: jest.fn(),
-            setContext: jest.fn()
-        };
-        healthCheckService = new HealthCheckService(loggerServiceMock);
-
+        jest.clearAllMocks();
+        healthCheckService = new HealthCheckService(MockLoggerService);
+        mockLoggerService = MockLoggerService;
         jest.useFakeTimers();
         jest.spyOn(global, 'setInterval');
         jest.spyOn(global, 'clearInterval');
     });
 
     it('should set logger context on construction', () => {
-        expect(loggerServiceMock.setContext).toHaveBeenCalledWith({ module: 'services' });
+        expect(mockLoggerService.setContext).toHaveBeenCalledWith({ module: 'services' });
     });
 
     it('should log start message and set interval on start', async () => {
         await healthCheckService.start(1000);
-        expect(loggerServiceMock.info).toHaveBeenCalledWith('Starting health check service...');
+        expect(mockLoggerService.info).toHaveBeenCalledWith('Starting health check service...');
         expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
     });
 
@@ -40,18 +35,18 @@ describe('HealthCheckService', () => {
     it('should log stop message and clear timer on stop', async () => {
         await healthCheckService.start(1000);
         await healthCheckService.stop();
-        expect(loggerServiceMock.info).toHaveBeenCalledWith('Stopping health check service...');
+        expect(mockLoggerService.info).toHaveBeenCalledWith('Stopping health check service...');
         expect(clearInterval).toHaveBeenCalledTimes(1);
     });
 
     it('should add health check on registerHealthCheck', () => {
-        const healthCheckMock: IHealthCheck = {
+        const mockHealthCheck: IHealthCheck = {
             name: 'MockHealthCheck',
             getStatus: jest.fn(),
             check: jest.fn()
         };
 
-        healthCheckService.registerHealthCheck(healthCheckMock);
-        expect(healthCheckService.getHealthChecks()).toContain(healthCheckMock);
+        healthCheckService.registerHealthCheck(mockHealthCheck);
+        expect(healthCheckService.getHealthChecks()).toContain(mockHealthCheck);
     });
 });
