6b2f489187b7ac4a66dfc403973f02bfdfb242b3
Marius Begby <mariusbegby@gmail.com>
2024-07-27 12:58:50 +0200

    fix private property

--- src/core/_types/ICoreValidator.ts
+++ src/core/_types/ICoreValidator.ts
@@ -1,10 +1,4 @@
-import type { ILoggerService } from '@type/insights/ILoggerService';
-import type { IConfig } from 'config';
-
 export interface ICoreValidator {
-    _logger: ILoggerService;
-    _config: IConfig;
-
     validateEnvironmentVariables(): void;
     validateConfiguration(): void;
     checkDependencies(): void;
