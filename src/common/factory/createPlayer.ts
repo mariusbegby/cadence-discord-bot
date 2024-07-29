de0dd943e14e5da092396b847214647b16abc7e6
Marius Begby <mariusbegby@gmail.com>
2024-07-29 07:09:05 +0200

    try to fix palyerSkip bug

--- src/common/factory/createPlayer.ts
+++ src/common/factory/createPlayer.ts
@@ -1,57 +1,65 @@
-import config from 'config';
-import { IPRotationConfig, Player } from 'discord-player';
-import { SpotifyExtractor } from '@discord-player/extractor';
-import { loggerService, Logger } from '../services/logger';
-import { CreatePlayerParams } from '../../types/playerTypes';
-import { YoutubeiExtractor } from 'discord-player-youtubei';
+import config from "config";
+import { IPRotationConfig, Player } from "discord-player";
+import { SpotifyExtractor } from "@discord-player/extractor";
+import { loggerService, Logger } from "../services/logger";
+import { CreatePlayerParams } from "../../types/playerTypes";
+import { YoutubeiExtractor } from "discord-player-youtubei";
 
-export const createPlayer = async ({ client, executionId }: CreatePlayerParams): Promise<Player> => {
-    const logger: Logger = loggerService.child({
-        module: 'utilFactory',
-        name: 'createPlayer',
-        executionId: executionId,
-        shardId: client.shard?.ids[0]
-    });
+export const createPlayer = async ({
+	client,
+	executionId,
+}: CreatePlayerParams): Promise<Player> => {
+	const logger: Logger = loggerService.child({
+		module: "utilFactory",
+		name: "createPlayer",
+		executionId: executionId,
+		shardId: client.shard?.ids[0],
+	});
 
-    const ipRotationConfig = config.get<IPRotationConfig>('ipRotationConfig');
+	const ipRotationConfig = config.get<IPRotationConfig>("ipRotationConfig");
 
-    try {
-        logger.debug('Creating discord-player player...');
+	try {
+		logger.debug("Creating discord-player player...");
 
-        const player: Player = new Player(client, {
-            useLegacyFFmpeg: false,
-            skipFFmpeg: false,
-            ipconfig: ipRotationConfig
-        });
+		const player: Player = new Player(client, {
+			useLegacyFFmpeg: false,
+			skipFFmpeg: false,
+			ipconfig: ipRotationConfig,
+		});
 
-        function getAuthArrayFromEnv(): string[] {
-            return Object.keys(process.env)
-                .filter((v) => v.startsWith('YT_EXTRACTOR_AUTH'))
-                .map((k) => process.env[k])
-                .filter((v) => v !== undefined);
-        }
+		function getAuthArrayFromEnv(): string[] {
+			return Object.keys(process.env)
+				.filter((v) => v.startsWith("YT_EXTRACTOR_AUTH"))
+				.map((k) => process.env[k])
+				.filter((v) => v !== undefined);
+		}
 
-        // Testing out new youtube extractor
-        await player.extractors.register(YoutubeiExtractor, {
-            //authentication: process.env.YT_EXTRACTOR_AUTH || '',
-            rotator: {
-                rotationStrategy: 'shard',
-                authentications: getAuthArrayFromEnv() || [''],
-                currentShard: client.shard?.ids[0] || 0
-            }
-        });
+		// Testing out new youtube extractor
+		await player.extractors.register(YoutubeiExtractor, {
+			//authentication: process.env.YT_EXTRACTOR_AUTH || '',
+			rotator: {
+				rotationStrategy: "shard",
+				authentications: getAuthArrayFromEnv() || [""],
+				currentShard: client.shard?.ids[0] || 0,
+			},
+			streamOptions: {
+				useClient: "ANDROID",
+			},
+		});
 
-        // make player accessible from anywhere in the application
-        // primarily to be able to use it in broadcastEval and other sharding methods
-        // @ts-ignore
-        global.player = player;
+		// make player accessible from anywhere in the application
+		// primarily to be able to use it in broadcastEval and other sharding methods
+		// @ts-ignore
+		global.player = player;
 
-        await player.extractors.loadDefault((ext) => !['YouTubeExtractor'].includes(ext));
-        logger.trace(`discord-player loaded dependencies:\n${player.scanDeps()}`);
+		await player.extractors.loadDefault(
+			(ext) => !["YouTubeExtractor"].includes(ext),
+		);
+		logger.trace(`discord-player loaded dependencies:\n${player.scanDeps()}`);
 
-        return player;
-    } catch (error) {
-        logger.error(error, 'Failed to create discord-player player');
-        throw error;
-    }
+		return player;
+	} catch (error) {
+		logger.error(error, "Failed to create discord-player player");
+		throw error;
+	}
 };
